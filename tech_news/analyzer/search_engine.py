974cccb05fd81e4e8849b96cc7889e4769c31194
Lizandra Debussy <lizandradebussy@hotmail.com>
2023-03-17 11:47:05 -0300

    Implementa a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,7 +16,20 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        formatted_date = date_obj.strftime("%d/%m/%Y")
+
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    query = {"timestamp": formatted_date}
+    search_news_db = search_news(query)
+    result = []
+
+    for news in search_news_db:
+        result.append((news["title"], news["url"]))
+    return result
 
 
 # Requisito 9
