e0bdd7a9c53c79597b16c4df9006c2c500bf2726
Lizandra Debussy <lizandradebussy@hotmail.com>
2023-11-29 21:36:01 -0300

    feat: Implementa  a função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,29 @@
+from tech_news.database import search_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    all_news = search_news({})
+
+    all_categories = []
+    for news in all_news:
+        category = news.get("category")
+        if category:
+            all_categories.append(category)
+
+    categories_count = Counter(all_categories)
+
+    # Ordenar as categorias por popularidade e em ordem alfabética
+    sorted_categories = sorted(
+        categories_count.items(),
+        # Ordenar por contagem decrescente e depois por ordem alfabética
+        key=lambda x: (-x[1], x[0])
+    )
+
+    # Retornar as cinco categorias mais populares ou menos se houver menos de 5
+    top_categories = []
+    for category, _ in sorted_categories[:5]:
+        top_categories.append(category)
+
+    return top_categories
